eabf1d59d39918a7c49f3765f9f2b9ebfe872d26
taejin-seong <sungtj0130@naver.com>
2022-04-23 16:39:40 +0900

    p30~p31

--- 01.네트워크 프로그래밍과 소켓의 이해/01-2. 리눅스 기반 파일 조작하기/low_open.c
+++ 01.네트워크 프로그래밍과 소켓의 이해/01-2. 리눅스 기반 파일 조작하기/low_open.c
@@ -1,3 +1,4 @@
+/* Linux */
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -9,14 +10,14 @@ int main(void)
     int fd;
     char buf[] = "Let's go\n";
 
-    fd = open("data.txt", O_CREAT|O_WRONLY|O_TRUNC);
+    fd = open("data.txt", O_CREAT|O_WRONLY|O_TRUNC); // 아무것도 저장되어있지 않은 새로운 파일이 생성되어 쓰기만 가능해짐. 물론 이미 data.txt라는 이름의 파일이 존재한다면, 이 파일의 모든 데이터는 지워져버림
     if(fd == -1)
     {
         error_handling("open() error!");
     }
     printf("file descriptor : %d \n", fd);
 
-    if (write(fd, buf, sizeof(buf)) == -1)
+    if (write(fd, buf, sizeof(buf)) == -1) //fd에 저장된 파일 디스크립터에 해당하는 buf에 저장된 데이터를 전송함.
     {
         error_handling("write() error!");
     }
