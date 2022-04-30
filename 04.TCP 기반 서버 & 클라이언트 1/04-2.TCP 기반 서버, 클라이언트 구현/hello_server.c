0b4385f318dda5bd82c8b72f61730d6d85d2a13b
taejin-seong <sungtj0130@naver.com>
2022-04-30 17:44:17 +0900

    p103~p104

--- 04.TCP 기반 서버 & 클라이언트 1/04-2.TCP 기반 서버, 클라이언트 구현/hello_server.c
+++ 04.TCP 기반 서버 & 클라이언트 1/04-2.TCP 기반 서버, 클라이언트 구현/hello_server.c
@@ -24,35 +24,40 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    serv_sock = socket(PF_INET, SOCK_STREAM, 0); //socket 함수호출을 통한 소켓 생성
+    /* 서버 프로그램의 구현과정에서 제일 먼저 해야 할일이 소켓의 생성 */
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0); // 소켓을 생성, 단 이 때 생성되는 소켓은 아직 서버 소켓이라 부르기 이른 상태.
     if (serv_sock == -1)
     {
         error_handling("socket() error");
     }
 
+/* 35~43행 : 소켓의 주소할당을 위해 구조체 변수를 초기화하고 bind 함수를 호출 */
     memset(&serv_addr, 0 , sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(atoi(argv[1]));
 
-    if (bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == 1) //bind 함수호출을 통한 IP주소와 PORT번호 할당
+    if (bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == 1) 
     {
         error_handling("bind() error");
     }
 
-    if (listen(serv_sock, 5) == 1) //listen 함수호출, 이로써 소켓은 연결요청을 받아들일 수 있는 상태가 된다.
+/* 연결요청 대기상태로 들어가기 위해 listen 함수를 호출 */
+    if (listen(serv_sock, 5) == 1) // 연결요청 대기 큐의 크기를 5로 설정, 이제야 비로소 28행에서 생성한 소켓을 가리켜 서버 소켓이라 할 수 있음.
     {
         error_handling("listen() error");
     }
 
+/* accept 함수가 호출 되었으니, 대기 큐에서 첫 번째로 대기 중에 있는 연결요청을 참조하여 클라이언트와의 연결을 구성하고, 이 때 생성된 소켓의 파일 디스크립터를 반환 */
     clnt_addr_size = sizeof(clnt_addr);
-    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size); //연결요청 수락을 위한 accept 함수호출, 연결요청이 없는 상태에서 이 함수가 호출되면, 연결요청이 있을 때까지 함수는 반환하지 않음
+    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size); //만약 이 함수가 호출되었을 때 대기 큐가 비어있는 상태라면, 대기 큐가 찰 때까지,(클라이언트 연결요청이 들어 올 때 까지) accept 함수는 반환하지 않음.
     if(clnt_sock == -1)
     {
         error_handling("accpet() error");
     }
 
-    write(clnt_sock, message, sizeof(message)); //write 함수호출을 통하여 데이터를 전송
+/* write 함수호출을 통해 클라이언트에게 데이터를 전송, 그리고 close 함수호출을 통해서 연결을 끊음. */
+    write(clnt_sock, message, sizeof(message)); 
     close(clnt_sock);
     close(serv_sock);
 
